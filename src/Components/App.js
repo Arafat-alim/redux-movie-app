59c25aaaa08fa736002980258e5f8af6344dfc5e
Arafat <arafat97alim@gmail.com>
2021-08-29 00:58:50 +0530

    added two more comp - navbar and moviecard with styling.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,24 @@
+import { data } from "../data";
+import Navbar from "./Navbar";
+import Moviecard from "./Moviecard";
 function App() {
-  return <div className="App">Project Setup</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourite</div>
+        </div>
+
+        <div className="list">
+          {data.map((movie) => {
+            return <Moviecard movie={movie} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Moviecard.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Moviecard extends Component {
+  render() {
+    console.log(this.props);
+    const { movie } = this.props;
+    return (
+      <div className="movie-card">
+        <div className="left">
+          <img src={movie.Poster} alt="movie-poster" />
+        </div>
+        <div className="right">
+          <div className="title">{movie.Title}</div>
+          <div className="plot">{movie.Plot}</div>
+          <div className="footer">
+            <div className="rating">{movie.imdbRating}</div>
+            <button className="favourite-btn">Add to Favourite</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Moviecard;
