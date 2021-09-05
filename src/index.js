64c2f63c26af6623d8a56ab4ef96a443dc3b032f
Arafat <arafat97alim@gmail.com>
2021-09-05 22:31:57 +0530

    middleware basic concept added.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import "./index.css";
 import App from "./Components/App";
 // import movies from "./reducer/index";
 import rootReducer from "./reducer/index";
 
+// creating a middleware named  logger
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      //middleware codes
+      console.log("ACTION_TYPES - ", action.type);
+      next(action); //! Important to call next, warna yehi pe stuck hojyga code
+    };
+  };
+};
 //creating a store
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("store", store);
 // console.log("Before Dispatch State", store.getState());
 
